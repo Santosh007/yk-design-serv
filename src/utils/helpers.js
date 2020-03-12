96f3eb201a9c3485dd964e8d6ef0e901a2950439
Santosh <santoshreddy8341@live.com>
2020-03-12 20:51:43 +0530

    url formatter
    URL formatter

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,11 +1,12 @@
 exports.inclueImgLink = (req, res) => {
-  let url = "https://" + req.get("host") + "/apis/archi";
+  let url = "https://" + req.get("host");
+
   return res.data.files.map(file => {
     let rFile = {};
     rFile["key"] = file.id;
     rFile["name"] = file.name;
     rFile["desc"] = file.description;
-    rFile["image"] = new URL("/" + file.id, url);
+    rFile["image"] = new URL("/apis/archi/" + file.id, url).href;
     rFile["alt"] = file.name;
     return rFile;
   });
